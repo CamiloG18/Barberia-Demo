46f37d095013be0f763c0fb3f6d4dcdfd91eeae6
CamiloG18 <cmlogmztrbo@gmail.com>
2026-03-14 15:31:47 -0500

    ultimo jaja

--- src/data/business.js
+++ src/data/business.js
@@ -10,12 +10,6 @@ const business = {
     "Arreglo de barba",
     "Afeitado tradicional"
   ],
-
-  gallery: [
-    "https://images.unsplash.com/photo-1503951914875-452162b0f3f1",
-    "https://images.unsplash.com/photo-1621605815971-fbc98d665033",
-    "https://images.unsplash.com/photo-1517832606299-7ae9b720a186"
-  ]
 };
 
 export default business;
